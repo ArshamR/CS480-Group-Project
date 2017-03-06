f27c189c8069c17a90d5a543240d67932d6fbf89
Arsham Ravanipour <arsham0527@gmail.com>
2017-03-05 19:59:14 -0800

    Updated to output to file

--- detect.sh
+++ detect.sh
@@ -6,35 +6,41 @@ if [ $# -ne 1 ]
 	exit 1;
 fi
 
-
+rm results.txt
+declare -a sourceIps
 count=0
 while read p;
 do
 	timeS=`echo "$p" | awk '{ print $3 }'`
 	port=`echo "$p" | awk '{ print $7 }'`
 	ip=`echo "$p" | awk '{ print $9 }'`
+	sourceIp=`echo "$p" | awk '{ print $8 }'`
 
-	if [ "$timeS" = "$timePrev" -a "$port" = "$portPrev" -a "$ip = $ipPrev" ]
+	if [ "$timeS" = "$timePrev" -a "$port" = "$portPrev" -a "$ip" = "$ipPrev" ]
 		then
+			sourceIps[$count]=$sourceIp
 			count=`expr $count + 1`
-	
-else
-	let count=0
-fi
 
-if [ $count -ge 10 ]
+
+	elif [ $count -ge 10 ]
 	then 
-	echo "**************DDOS ATTACK DETECTED*******"
-fi
+		echo "**************DDOS ATTACK DETECTED*******"
+		printf '%s\n' "${sourceIps[@]}" >> results.txt
+		printf '%s\n' "$portPrev" >> results.txt
+		printf '%s\n' "$timePrev" >> results.txt
+		printf '%s\n' "*" >> results.txt
 
-	echo "Count: $count"
-	echo "Timestamp: $timeS"
-	echo "Port: $port"
-	echo "Ip: $ip"
+		let count=0
+		sourceIps=()
+	else
+		let count=0
+		sourceIps=()
+	fi
+
+
 	timePrev=$timeS
 	portPrev=$port
 	ipPrev=$ip
-
 
 done < ddos.txt
 
